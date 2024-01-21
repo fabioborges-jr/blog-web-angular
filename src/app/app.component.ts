731b3ab27015e65959d938e8785d8c5f9c482f37
Fabio <fabioborgesouzajunior@gmail.com>
2024-01-21 02:09:49 -0300

    :lipstick: feat: Styling background and Heading Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeadingComponent } from './src/components/heading/heading.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeadingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/src/components/heading/heading.component.ts
+++ src/app/src/components/heading/heading.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-heading',
+  selector: 'Heading',
   standalone: true,
   imports: [],
   templateUrl: './heading.component.html',
