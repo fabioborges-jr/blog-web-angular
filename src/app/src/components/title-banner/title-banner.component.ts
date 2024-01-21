c4a2db78c9d585df65b2adddd4590d3c2c253ecb
Fabio <fabioborgesouzajunior@gmail.com>
2024-01-21 15:44:37 -0300

    :sparkles: feat: TitleBanner component created

---
+++ src/app/src/components/title-banner/title-banner.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-title-banner',
+  standalone: true,
+  imports: [],
+  templateUrl: './title-banner.component.html',
+  styleUrl: './title-banner.component.scss'
+})
+export class TitleBannerComponent {
+
+}
