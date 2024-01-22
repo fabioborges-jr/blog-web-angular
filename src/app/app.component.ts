0118f88e1553a4a0ad3c13e64e11a57b5a56b021
Fabio <fabioborgesouzajunior@gmail.com>
2024-01-22 18:58:11 -0300

    :sparkles: feat: bigCard component created, styled and finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeadingComponent } from './src/components/heading/heading.component';
 import { TitleBannerComponent } from './src/components/title-banner/title-banner.component';
+import { BigCardComponent } from './src/components/big-card/big-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeadingComponent, TitleBannerComponent],
+  imports: [RouterOutlet, HeadingComponent, TitleBannerComponent, BigCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
