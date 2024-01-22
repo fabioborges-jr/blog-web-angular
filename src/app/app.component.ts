6d96dd954558ad4a08cce6efdf82dd02edb751dc
Fabio <fabioborgesouzajunior@gmail.com>
2024-01-21 21:42:42 -0300

    :sparkles: feat: TitleBanner component finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeadingComponent } from './src/components/heading/heading.component';
+import { TitleBannerComponent } from './src/components/title-banner/title-banner.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeadingComponent],
+  imports: [RouterOutlet, HeadingComponent, TitleBannerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/src/components/title-banner/title-banner.component.ts
+++ src/app/src/components/title-banner/title-banner.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-title-banner',
+  selector: 'TitleBanner',
   standalone: true,
   imports: [],
   templateUrl: './title-banner.component.html',
