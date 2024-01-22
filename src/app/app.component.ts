a041dbcba440a738825e2673ec56571769998246
Fabio <fabioborgesouzajunior@gmail.com>
2024-01-22 19:02:32 -0300

    :sparkles: feat: SmallCard component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeadingComponent } from './src/components/heading/heading.component';
 import { TitleBannerComponent } from './src/components/title-banner/title-banner.component';
 import { BigCardComponent } from './src/components/big-card/big-card.component';
+import { SmallCardComponent } from './src/components/small-card/small-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeadingComponent, TitleBannerComponent, BigCardComponent],
+  imports: [RouterOutlet, HeadingComponent, TitleBannerComponent, BigCardComponent, SmallCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/src/components/small-card/small-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'SmallCard',
+  standalone: true,
+  imports: [],
+  templateUrl: './small-card.component.html',
+  styleUrl: './small-card.component.scss'
+})
+export class SmallCardComponent {
+
+}
