cbb6c471cab645c0d88771ceaaf45dbc7464a5c4
Fabio <fabioborgesouzajunior@gmail.com>
2024-01-25 17:31:37 -0300

    :sparkles: feat: Pages router added

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-content',
+  selector: 'ContentPage',
   standalone: true,
   imports: [],
   templateUrl: './content.component.html',
