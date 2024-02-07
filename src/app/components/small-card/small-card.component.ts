bab6a85a1a763f47150a195776c8dac4b6c6b518
Fabio <fabioborgesouzajunior@gmail.com>
2024-02-07 19:33:08 -0300

    :sparkles: feat: SmallCardComponent renders movies mocks

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Movie } from '../../types/Movie';
+import { NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'SmallCard',
   standalone: true,
-  imports: [],
+  imports: [
+    NgFor,
+    FormsModule],
   templateUrl: './small-card.component.html',
   styleUrl: './small-card.component.scss'
 })
 export class SmallCardComponent {
-
+  @Input() movie?:Movie
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,6 +5,8 @@ import { BigCardComponent } from '../../components/big-card/big-card.component';
 import { SmallCardComponent } from '../../components/small-card/small-card.component';
 import { movies } from "../../data/dataFake"
 import { Movie } from '../../types/Movie';
+import { NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'HomePage',
@@ -13,12 +15,15 @@ import { Movie } from '../../types/Movie';
     HeadingComponent,
     TitleBannerComponent,
     BigCardComponent,
-    SmallCardComponent
+    SmallCardComponent,
+    NgFor,
+    FormsModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   movieBigCard:Movie|undefined=movies.shift()
-  // moviesSmallCards:Movie[]=movies.splice()
+  moviesSmallCards:Movie[]=movies
+  movies:Movie[]=movies
 }
