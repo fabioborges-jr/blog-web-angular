89bde84ead3cb0e2bb40d2b6e32429f9e3c1c1ec
Fabio <fabioborgesouzajunior@gmail.com>
2024-02-10 17:03:09 -0300

    :sparkles: feat: Implements function setValueToComponent() to contentComponentPage

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import {movies} from "../../data/dataFake"
 
 @Component({
   selector: 'ContentPage',
@@ -9,13 +10,22 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './content.component.scss'
 })
 export class ContentComponent implements OnInit {
+  id?: string|null
 
   constructor(
     private route:ActivatedRoute
   ){}
 
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.route.paramMap.subscribe(value=>
+      this.id=value.get("id"))
+
+    this.setValueToComponent(this.id)
+  }
+
+  setValueToComponent(id:string|null|undefined){
+    const movie=movies.filter(movie=>movie.id.toString()==id)
+    console.log(movie)
   }
 
 }
