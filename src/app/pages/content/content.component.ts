bfd59565e9b29bd8a0453e4b1f5d3addef36a71e
Fabio <fabioborgesouzajunior@gmail.com>
2024-02-10 15:01:38 -0300

    :sparkles: feat: Adds OnInit and ActivatedRoute to contentComponent

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'ContentPage',
@@ -7,6 +8,14 @@ import { Component } from '@angular/core';
   templateUrl: './content.component.html',
   styleUrl: './content.component.scss'
 })
-export class ContentComponent {
+export class ContentComponent implements OnInit {
+
+  constructor(
+    private route:ActivatedRoute
+  ){}
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
